c55949f7bdae2854f9efa838dda42adfbfbb1c67
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-13 21:03:52 +0100

    added 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,22 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-5 h-5 rounded-full bg-[#00769d]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#00769d]'>
+                Daniel</span></h1>
+                <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop user interfaces, <br className="sm:block hidden" /> 
+                prededictive project managment tools &nbsp;
+                <br className="sm:block hidden" /> 
+                and web applications.
+                </p>
         </div>
-      </div>
+        </div>
+        <ComputersCanvas />
+
     </section>
   )
 }
