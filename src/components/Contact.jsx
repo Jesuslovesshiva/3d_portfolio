15541b31730dd353345ec0c21403eed832f4d67f
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-20 23:15:43 +0100

    email now works in contact, uninstalled react-tilt --legacy-peer-deps, added react-parallax-tilt cuz of library peer dependency issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion'
 
+// SCfI0fEXQi9svHaIv
+// template_tz1rfif
+// service_irxt3b7
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +20,45 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+  
+    setForm({ ...form, [name]: value})
+  }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+    'service_irxt3b7', 
+    'template_tz1rfif',
+    { 
+      from_name: form.name,
+      to_name: 'Daniel Leitner',
+      from_email: form.email,
+      to_email: 'd.leonardleitner@gmail.com',
+      message: form.message
+    },
+    'SCfI0fEXQi9svHaIv'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Something went wrong')
+    })
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
@@ -62,7 +102,7 @@ const Contact = () => {
               <textarea
                 rows="7"
                 name="message"
-                value={form.name}
+                value={form.message}
                 onChange={handleChange}
                 placeholder="What do you want to say?"
                 className="bg-tertiary py-4 px-6 text-white rounded-lg outline-none placeholder:text-secondary border-none font-medium"
