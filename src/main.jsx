85bc69becd891dc7f62e8327708577f6f86d1f70
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-21 22:43:11 +0100

    added import TagManager from 'react-gtm-module'

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-TZM2M2P'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
