e7675586481c2b8cf6bca9b6261d2492e4b71b64
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-21 23:27:59 +0100

    added cookie button fade out

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Typography, Link } from '@material-ui/core';
 
 const CookieBanner = (props) => {
   const { message, linkText, linkUrl, buttonClasses, onAccept, onReject } = props;
 
+  const [showBanner, setShowBanner] = useState(true);
+
+  const handleAccept = () => {
+    setShowBanner(false);
+    onAccept();
+  };
+
+  const handleReject = () => {
+    setShowBanner(false);
+    onReject();
+  };
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 bg-gray-900 py-3 px-4 md:py-4 md:px-8 flex flex-col md:flex-row md:items-center justify-between">
+    <div className={`bg-black-100 fixed bottom-0 left-0 right-0 z-50 py-3 px-4 md:py-4 md:px-8 flex flex-col md:flex-row md:items-center justify-between ${showBanner ? 'opacity-100 transition-opacity' : 'opacity-0 transition-opacity duration-500 ease-out'}`}>
       <div className="flex items-center mb-2 md:mb-0">
         <Typography variant="body2" className="mr-2">{message}</Typography>
         {linkText && linkUrl && (
@@ -16,11 +28,11 @@ const CookieBanner = (props) => {
         )}
       </div>
       <div className="flex items-center">
-        <Button variant="contained" color="primary" className={buttonClasses} onClick={onAccept}>
+        <Button variant="contained" color="primary" className={buttonClasses} onClick={handleAccept}>
           Accept
         </Button>
         <div className="w-2"></div>
-        <Button variant="contained" color="secondary" className="ml-2" onClick={onReject}>
+        <Button variant="contained" color="secondary" className="ml-2" onClick={handleReject}>
           Reject
         </Button>
       </div>
