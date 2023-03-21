1b4a1cb118261af8c396c8818d436a600af6b74a
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-21 18:59:06 +0100

    installed npms with --legacy-peer-deps

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -3,11 +3,11 @@ import { useCookies } from 'react-cookie';
 import CookieBanner from 'react-cookie-banner';
 
 const CookieBannerComponent = () => {
-  const [cookies, setCookie] = useCookies(['my-website-cookies']);
+  const [cookies, setCookie] = useCookies(['3dportfolio-view-tracking-analytics-v1']);
 
   const handleAccept = () => {
-    // Set the 'my-website-cookies' cookie to true
-    setCookie('my-website-cookies', true, { path: '/' });
+    // Set the '3dportfolio-view-tracking-analytics' cookie to true
+    setCookie('3dportfolio-view-tracking-analytics-v1', true, { path: '/' });
 
     // Load the Google Tag Manager script
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TZM2M2P');
@@ -18,7 +18,7 @@ const CookieBannerComponent = () => {
       message="This website uses cookies to improve your experience."
       buttonMessage="Accept"
       onAccept={handleAccept}
-      cookie="my-website-cookies"
+      cookie="3dportfolio-view-tracking-analytics-v1"
     />
   );
 };
