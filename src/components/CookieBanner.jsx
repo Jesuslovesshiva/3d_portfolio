406d1906d6eeb5054fdcf7228acadb34ebab0d80
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-21 19:10:19 +0100

    added clonedelement

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useCookies } from 'react-cookie';
 import CookieBanner from 'react-cookie-banner';
+import cloneWithProps from 'react-addons-clone-with-props';
 
 const CookieBannerComponent = () => {
   const [cookies, setCookie] = useCookies(['3dportfolio-view-tracking-analytics-v1']);
@@ -13,13 +14,19 @@ const CookieBannerComponent = () => {
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TZM2M2P');
   };
 
+  // Clone the CookieBanner element and add a custom class
+  const clonedElement = cloneWithProps(<CookieBanner />, { className: 'my-custom-class' });
+
   return (
-    <CookieBanner
-      message="This website uses cookies to improve your experience."
-      buttonMessage="Accept"
-      onAccept={handleAccept}
-      cookie="3dportfolio-view-tracking-analytics-v1"
-    />
+    <>
+      <CookieBanner
+        message="This website uses cookies to improve your experience."
+        buttonMessage="Accept"
+        onAccept={handleAccept}
+        cookie="3dportfolio-view-tracking-analytics-v1"
+      />
+      {clonedElement}
+    </>
   );
 };
 
