1fc50be3694b5ea0a93838f79c82972d1e7dd3ad
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-22 09:32:25 +0100

    fixed assets

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,6 +2,5 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
-import CoffeeCanvas from "./Coffee2go"
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, CoffeeCanvas };
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
