5b6e67019c15597c82121653e4c53299bd421327
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-22 08:58:47 +0100

    deleted some comments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles'
-// import CoffeeCanvas  from './canvas/Coffee2go';
+
 import ComputersCanvas from './canvas/Computers';
 
 const Hero = () => {
@@ -25,11 +25,10 @@ const Hero = () => {
         </div>
         <div className="h-20"></div>
 
-        {/* <CoffeeCanvas /> */}
 
         <ComputersCanvas />
         
-        <div className='absolute xs:bottom-10 bottom 32 w-full flex justify-center items-center'>
+        {/* <div className='absolute xs:bottom-10 bottom 32 w-full flex justify-center items-center'>
           <a href="#about">
             <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
               <motion.dev
@@ -45,7 +44,7 @@ const Hero = () => {
                />
             </div>
           </a>
-        </div>
+        </div> */}
 
     </section>
   )
